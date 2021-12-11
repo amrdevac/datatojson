1d116d1df9f1fd5b323d40ea244b31d047808711
amrdevac <amrullahdev@yahoo.com>
2021-12-12 01:43:22 +0700

    change the flow of  converting data to put the file inside per folder as table names

--- src/ConvertDataCommand.php
+++ src/ConvertDataCommand.php
@@ -38,20 +38,45 @@ class ConvertDataCommand extends Command
      */
     public function handle()
     {
-
-        $path = public_path("amrdevac");
         $table = $this->argument('table');
+        $path = public_path("amrdevac/$table");
         $filename = "file_$table.json";
 
         $this->line("Converting To Data <fg=green>$table.</fg=green>");
         $this->line(" Your file name would be <bg=blue;>$filename</bg=blue;>");
-        if (!is_dir($path)) mkdir($path, 777);
 
-        $myfile = fopen($path . "/$filename", "w") or die("Unable to open file!");
-        $txt = DB::table($table)->get();
-        fwrite($myfile, $txt);
+        if (!is_dir($path)) {
+            mkdir($path, 777);
+        } else {
+            array_map('unlink', glob("$path/*"));
+            rmdir($path);
+            mkdir($path, 777);
+        }
 
+        $dataToInsert = [];
+
+        DB::table($table)->orderBy("created_at")->chunk(100000, function ($datas) use ($dataToInsert) {
+
+            $table = $this->argument('table');
+            $path = public_path("amrdevac/$table");
+
+            $filename = "file_$table.json";
+
+            $myfile = "";
+            if (file_exists($path . "/$filename")) {
+                $filename = "file_$table" . time() . ".json";
+                $myfile = fopen($path . "/$filename", "w") or die("Unable to open file!");
+            } else {
+                $myfile = fopen($path . "/$filename", "w") or die("Unable to open file!");
+            }
+
+            foreach ($datas as  $data) {
+                $dataToInsert[] = $data;
+            }
+
+            fwrite($myfile, json_encode($dataToInsert));
+        });
 
         return Command::SUCCESS;
     }
-}
+};
